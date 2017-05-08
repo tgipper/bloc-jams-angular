3ccb4200f84e8f011079a601e6ee2c5f4b60599b
Trevor Gipper <trevor.gipper@gmail.com>
2017-05-08 15:29:39 -0700

    complete checkpoint-10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-	function SongPlayer(Fixtures) {
+	function SongPlayer($rootScope, Fixtures) {
 		var SongPlayer = {};
 		
 		/**
@@ -30,6 +30,12 @@
 				preload: true
 			});
 			
+			currentBuzzObject.bind('timeupdate', function() {
+				$rootScope.$apply(function() {
+					SongPlayer.currentTime = currentBuzzObject.getTime();
+				});
+			});
+			
 			SongPlayer.currentSong = song;
 		};
 		
@@ -53,6 +59,12 @@
 		};
 		
 		SongPlayer.currentSong = null;
+		
+		/**
+		* @desc Current playback time (in seconds) of currently playing song
+		* @type {Number}
+		*/
+		SongPlayer.currentTime = null;
 		
 		/**
 		* @function play
@@ -116,10 +128,21 @@
 			}
 		}
 		
+		/**
+		* @function setCurrentTime
+		* @desc Set current time (in seconds) of currently playing song
+		* @param {Number} time
+		*/
+		SongPlayer.setCurrentTime = function(time) {
+			if (currentBuzzObject) {
+				currentBuzzObject.setTime(time);
+			}
+		};
+		
 		return SongPlayer;
 	}
 		
 angular
 	.module('blocJams')
-	.factory('SongPlayer', ['Fixtures', SongPlayer])
+	.factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer])
 })();
